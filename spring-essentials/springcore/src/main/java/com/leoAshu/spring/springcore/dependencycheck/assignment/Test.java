8eba00464f08e1b97b9b9ea189c81a4ea423837f
leoAshu <ashutosh.ojha2009@gmail.com>
2024-02-02 17:30:19 -0800

    spring essentials: dependency check and scope assignment

---
+++ spring-essentials/springcore/src/main/java/com/leoAshu/spring/springcore/dependencycheck/assignment/Test.java
@@ -0,0 +1,20 @@
+package com.leoAshu.spring.springcore.dependencycheck.assignment;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/leoAshu/spring/springcore/dependencycheck/assignment/config.xml");
+		
+		University university1 = (University) ctx.getBean("university");
+		System.out.println(university1);
+		System.out.println(university1.hashCode());
+		
+		University university2 = (University) ctx.getBean("university");
+		System.out.println(university2);
+		System.out.println(university2.hashCode());
+	}
+
+}
