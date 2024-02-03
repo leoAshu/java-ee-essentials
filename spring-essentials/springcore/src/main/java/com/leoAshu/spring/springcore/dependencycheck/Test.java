dd6652cdb1b176a105ea47fb21ebf394d44fc464
leoAshu <ashutosh.ojha2009@gmail.com>
2024-02-02 17:07:20 -0800

    spring essentials: dependency check

---
+++ spring-essentials/springcore/src/main/java/com/leoAshu/spring/springcore/dependencycheck/Test.java
@@ -0,0 +1,14 @@
+package com.leoAshu.spring.springcore.dependencycheck;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/leoAshu/spring/springcore/dependencycheck/config.xml");
+		Prescription prescription = (Prescription) ctx.getBean("prescription");
+		System.out.println(prescription);
+	}
+
+}
